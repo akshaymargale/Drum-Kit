55b53b6b16f82eadc2cc982031dc7355f70f32a1
Akshay Margale <akshayprofacc@gmail.com>
2021-10-22 13:54:34 +0530

    Added sound to particular button and bg image

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 
-// $(".btn").click(function(){
-//     //  $("#key").text();
-//     const btn=this;
-//     console.log($(this).children('.btn-container').children('.btn-txt')[0].innerHTML);
-// });
-// $(document).keypress(function(event){
-//     console.log(event.key);
-// });
+function makeSound(char)
+{
+    switch(char)
+    {
+        case 'A':
+            var audio = new Audio('sound/bassdrum.mp3');
+            audio.play();
+            break;
+        case 'S':
+            var audio = new Audio('sound/hihat.mp3');
+            audio.play();
+            break;
+        case 'D':
+            var audio = new Audio('sound/snaredrum.mp3');
+            audio.play();
+            break;
+        case 'F':
+            var audio = new Audio('sound/tomdrum.mp3');
+            audio.play();
+            break;
+        case 'G':
+            var audio = new Audio('sound/floortomdrum.mp3');
+            audio.play();
+            break;
+    }
+}
+$(".btn").click(function(){
+    const char =  $(this).children('.btn-container').children('.btn-txt')[0].innerHTML;
+    makeSound(char);
+});
+
+$(document).keypress(function(event){
+   const char = event.key.toUpperCase();
+    makeSound(char);
+});
